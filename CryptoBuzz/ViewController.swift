e86783dad6519f81538bdd7f2f393ca3b4e0ea9d
Ajay Choudhary <ajaychoudhary19990@gmail.com>
2019-12-12 08:48:55 +0530

    First Commit

---
+++ CryptoBuzz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CryptoBuzz
+//
+//  Created by Ajay Choudhary on 12/12/19.
+//  Copyright © 2019 Ajay Choudhary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
